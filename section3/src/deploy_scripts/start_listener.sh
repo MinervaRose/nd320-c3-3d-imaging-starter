b72959bd46ef7d074d9987634417ebdb2777905f
MinervaRose <39020690+MinervaRose@users.noreply.github.com>
2025-04-17 23:03:17 +0200

    Update start_listener.sh

--- section3/src/deploy_scripts/start_listener.sh
+++ section3/src/deploy_scripts/start_listener.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Register the DICOM routing script with Orthanc
 curl -X POST http://localhost:8042/tools/execute-script --data-binary @route_dicoms.lua -v
-sudo storescp 106 -v -aet HIPPOAI -od /home/workspace/section3/src/routed --sort-on-study-uid st
+
+# Start the listener to receive routed DICOMs
+sudo storescp 106 -v -aet HIPPOAI -od /home/workspace/src/routed --sort-on-study-uid st
